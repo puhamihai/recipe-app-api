fe0bd6d76a90ad5b69abe53302f60a41a2e7102c
Puha <mihai.puha@apstra.com>
2022-08-07 10:28:10 +0300

    Added user app

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,44 @@
-from django.contrib import admin    # noqa
+"""Django admin customization"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import   UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as translate
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the Admin Pages for users."""
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            translate('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (translate('Important Dates'), {'fields': ('last_login', )}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+admin.site.register(models.User, UserAdmin)
+
